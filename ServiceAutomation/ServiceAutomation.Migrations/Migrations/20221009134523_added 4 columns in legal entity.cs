1a936d1311e9bdb731b37645d88c1f0b2cd96b18
Tim Yakubovskiy <tim.yakubovski@gmail.com>
2022-10-09 16:47:24 +0300

    йцуйцуйцуйц

---
+++ ServiceAutomation/ServiceAutomation.Migrations/Migrations/20221009134523_added 4 columns in legal entity.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServiceAutomation.DataAccess.Migrations.Migrations
+{
+    public partial class added4columnsinlegalentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CertificateDateIssue",
+                table: "LegalUserOrganizationsData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityNumber",
+                table: "LegalUserOrganizationsData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                table: "LegalUserOrganizationsData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationAuthority",
+                table: "LegalUserOrganizationsData",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CertificateDateIssue",
+                table: "LegalUserOrganizationsData");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityNumber",
+                table: "LegalUserOrganizationsData");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "LegalUserOrganizationsData");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationAuthority",
+                table: "LegalUserOrganizationsData");
+        }
+    }
+}
